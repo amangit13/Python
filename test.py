36b0db5e7f0dde7aa0630e960f556f208ccb5fe3
amangit13 <43822821+amangit13@users.noreply.github.com>
2021-03-15 21:52:04 -0700

    file io test

--- test.py
+++ test.py
@@ -4,3 +4,14 @@ class Dog:
 
 dog = Dog("bob")
 print(dog.name)
+hex = bytearray([0xEF]*10)
+print (hex)
+file = open("rom.bin", "wb")
+file.write(hex)
+file.close()
+
+file = open("rom.bin", "rb")
+content = file.read()
+file.close()
+
+print (content)
