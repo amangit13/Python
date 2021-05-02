5365ab1459f0aa8ea37b3bdfc42f0727ca37a1ce
amangit13 <43822821+amangit13@users.noreply.github.com>
2021-05-01 23:37:20 -0700

    Update Serial.py

--- Serial.py
+++ Serial.py
@@ -3,7 +3,17 @@ import time
 
 print ("hello")
 ser = serial.Serial("COM4",9600)
-
+i=0
 while True:
     content = ser.read()
-    print (chr(content[0]), end='')
+    if (content[0] == 32):
+        print ("\t", end='')
+        i+=1
+    else:
+        if i>3:
+            i=0
+            print ("")
+        print (hex(content[0]), end='')
+        print (" ",end="")
+
+        input()
